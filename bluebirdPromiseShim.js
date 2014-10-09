58f94ad182302acc9c29920a509c35c8986249c2
syzer <syzer3@gmail.com>
2014-10-09 12:07:42 +0200

    added deffered with bluebird

---
+++ bluebirdPromiseShim.js
@@ -0,0 +1,66 @@
+/**
+ * Created by syzer on 10/8/2014.
+ */
+var Promise = require('bluebird');
+
+var a = 42;
+
+function defer() {
+    var resolve, reject;
+    var promise = new Promise(function () {
+        resolve = arguments[0];
+        reject = arguments[1];
+    });
+    return {
+        resolve: resolve,
+        reject: reject,
+        promise: promise
+    };
+}
+
+function resolveMe(a) {
+    return defer();
+}
+
+var first = resolveMe(a);
+var second = resolveMe(a + 1);
+
+first.promise.then(function (data) {
+    console.log('resolved first', data);
+});
+
+second.promise.then(function (data) {
+    console.log('resolved second', data);
+});
+
+// all
+Promise
+    .all([first.promise, second.promise])
+    .then(function (data) {
+        console.log(data);
+    });
+
+// join
+Promise
+    .join(first.promise, second.promise, function (data, data2) {
+        console.log('done using join', data, data2);
+        return [data, data2];
+    })
+    .then(function (data) {
+        console.log(data);
+    });
+
+Promise
+    .any([first.promise, second.promise])
+    .then(function (data) {
+        console.log('done using any promise', data);
+    });
+
+Promise
+    .some([first.promise, second.promise], 2)
+    .then(function (data) {
+        console.log('done using some', data);
+    });
+
+first.resolve(1);
+second.resolve(2);
